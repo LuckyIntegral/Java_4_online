d5bd0607a0e3d82a90dd3efd6f999086f0246a5d
Vitalik <frantsv2004@gmail.com>
2023-02-04 13:59:16 +0100

    added Main class

---
+++ hw_10_json_and_crud/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.controller.UserInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.start();
+    }
+}
