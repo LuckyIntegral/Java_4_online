7ff2e8e8aacc3716c7f01f3051d965e1ad2990a3
Vitalik <frantsv2004@gmail.com>
2023-02-07 19:46:15 +0100

    added controller contract

---
+++ hw_10_crud_with_files/src/main/java/ua/com/alevel/controller/ControllerContract.java
@@ -0,0 +1,23 @@
+package ua.com.alevel.controller;
+
+import java.io.BufferedReader;
+
+public interface ControllerContract {
+    void createBook(BufferedReader reader);
+    void attachAuthorToBook(BufferedReader reader);
+    void deleteAuthorFromBook(BufferedReader reader);
+    void readBookById(BufferedReader reader);
+    void readAllBooks();
+    void upgradeBook(BufferedReader reader);
+    void deleteBook(BufferedReader reader);
+    void createAuthor(BufferedReader reader);
+    void attachBookToAuthor(BufferedReader reader);
+    void deleteBookFromAuthor(BufferedReader reader);
+    void readAuthorById(BufferedReader reader);
+    void readAllAuthors();
+    void upgradeAuthor(BufferedReader reader);
+    void deleteAuthor(BufferedReader reader);
+    void deleteAllBooks();
+    void deleteAllAuthors();
+    void exitProgram();
+}
