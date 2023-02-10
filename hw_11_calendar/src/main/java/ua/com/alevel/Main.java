1fef56d215dfc4d0f79b8f9d1ad4aead6fb3611a
Vitalik <frantsv2004@gmail.com>
2023-02-10 22:26:48 +0100

    add Main class

--- hw_11_calendar/src/main/java/ua/com/alevel/Main.java
+++ hw_11_calendar/src/main/java/ua/com/alevel/Main.java
@@ -1,7 +1,10 @@
 package ua.com.alevel;
 
+import ua.com.alevel.controller.CalendarController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalendarController controller = new CalendarController();
+        controller.start();
     }
 }
